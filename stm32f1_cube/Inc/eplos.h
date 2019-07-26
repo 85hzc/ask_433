f8bb06cbfc0c3d4fe39cc5d74b599ece4e8574f8
85hzc <85hzc@163.com>
2019-07-26 17:27:10 +0800

    osram EPLOS driver and gpio map develop。
    Signed-off-by: 85hzc <85hzc@163.com>

---
+++ stm32f1_cube/Inc/eplos.h
@@ -0,0 +1,46 @@
+
+#ifndef __EPLOS_H
+#define __EPLOS_H
+
+#define I2C_ADDR01                  0x20  //A1-3  pulldown
+#define I2C_ADDR23                  0x21
+
+#define EPLOS_I2C_ADD01             0x00  //010 000 0B=0x20
+#define EPLOS_CFG01                 0x01
+#define EPLOS_STATUS01              0x02
+#define EPLOS_I2C_MON01             0x03
+#define EPLOS_I2C_CS01              0x04
+#define EPLOS_SCI_MON01             0x05
+#define EPLOS_DIAG_OUT_SEL01        0x06
+#define EPLOS_DIAG_OUT_DAT0         0x07
+#define EPLOS_DIAG_OUT_DAT1         0x08
+#define EPLOS_DIAG_TM_DAT0          0x09
+#define EPLOS_DIAG_TM_DAT1          0x0A
+#define EPLOS_DIAG_TM_CFG01         0x0B
+#define EPLOS_DIAG_VCP_DAT0         0x0C
+#define EPLOS_DIAG_VCP_DAT1         0x0D
+#define EPLOS_DIAG_VCP_CFG0         0x0E
+#define EPLOS_DIAG_VCP_CFG1         0x0F
+
+#define EPLOS_I2C_ADD23             0x00  //010 000 1B=0x21
+#define EPLOS_CFG23                 0x01
+#define EPLOS_STATUS23              0x02
+#define EPLOS_I2C_MON23             0x03
+#define EPLOS_I2C_CS23              0x04
+#define EPLOS_SCI_MON23             0x05
+#define EPLOS_DIAG_OUT_SEL23        0x06
+#define EPLOS_DIAG_OUT_DAT2         0x07
+#define EPLOS_DIAG_OUT_DAT3         0x08
+#define EPLOS_DIAG_TM_DAT2          0x09
+#define EPLOS_DIAG_TM_DAT3          0x0A
+#define EPLOS_DIAG_TM_CFG23         0x0B
+#define EPLOS_DIAG_VCP_DAT2         0x0C
+#define EPLOS_DIAG_VCP_DAT3         0x0D
+#define EPLOS_DIAG_VCP_CFG2         0x0E
+#define EPLOS_DIAG_VCP_CFG3         0x0F
+
+
+#define QT_PIXELS                   256
+
+#endif
+
